9ac3aed86f9c953ded9182ccf3a8e340b02f0e99
jimao <shihao.gu@perfma.com>
2024-08-13 18:55:36 +0800

    新的自定义指标

--- nodejs_exporter/app/routes/metrics.tsx
+++ nodejs_exporter/app/routes/metrics.tsx
@@ -1,12 +1,19 @@
 import { ActionFunctionArgs } from "@remix-run/node";
-import { collectDefaultMetrics, register } from "prom-client";
+import client from "prom-client";
 import Logger from "~/utils/logger";
 
-register.clear();
-collectDefaultMetrics();
+client.register.clear();
+client.register.registerMetric(new client.Gauge({
+  name: 'sin',
+  help: 'sin function',
+  collect() {
+    this.set((Math.sin(Math.floor(Date.now() / 2000) / 10) + 1) * 50);
+  },
+}));
+client.collectDefaultMetrics();
 
 export
 async function loader({ request }: ActionFunctionArgs) {
   Logger.info(request.url);
-  return new Response(await register.metrics(), { headers: { 'Content-Type': register.contentType } });
+  return new Response(await client.register.metrics(), { headers: { 'Content-Type': client.register.contentType } });
 }
